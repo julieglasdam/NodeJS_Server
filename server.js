b5ced9f1e2babd930deb7a7ed3013f912c8b2074
julieglasdam <julieglasdam@Julies-MacBook-Pro.local>
2017-09-08 11:44:49 +0200

    dutduy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var fs = require('fs');
 
 // Set up server using http variable
 var server = http.createServer(function(req, res){ // new function inside createServer function
@@ -6,16 +7,16 @@ var server = http.createServer(function(req, res){ // new function inside create
    	// First parameter is status code, second parameter is type of content
    	res.writeHead(200, {'Content-type' : 'text/json'}); 
 
-  var fs = require('fs');
-  var x;
-  fs.readFileSync('small.txt', 'utf-8', function (err, data) {
-   x = data;
-  });
+  
 
+  fs.readFileSync('small.txt', 'utf-8', function (err, data) {
     res.write('<h1>Heading</h1><p>Dis is text</p>');
     res.write(data);
    	// res.write('{"title" : "Hello JSON web API", "text" : "Text fra min API"}');
    	res.end();
+  });
+
+    
 });
 
 // Listen on port number
