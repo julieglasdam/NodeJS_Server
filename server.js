9d60215370c6c4e4e480baf771927ea85d933c2e
julieglasdam <julieglasdam@Julies-MacBook-Pro.local>
2017-09-08 11:22:58 +0200

    dddsady

--- server.js
+++ server.js
@@ -5,10 +5,21 @@ var server = http.createServer(function(req, res){ // new function inside create
    	res.setHeader('Access-Control-Allow-Origin', '*');
    	// First parameter is status code, second parameter is type of content
    	res.writeHead(200, {'Content-type' : 'text/json'}); 
-   	// res.write('<h1>Heading</h1><p>Dis is text</p>');
+
+var fs = require('fs');
+var text = fs.readFile('small.txt', 'utf-8', function (err, data) {
+	
+});
+
+    res.write('<h1>Heading</h1><p>Dis is text</p>');
+    res.write(text);
    	// res.write('{"title" : "Hello JSON web API", "text" : "Text fra min API"}');
    	res.end();
 });
 
 // Listen on port number
 server.listen(process.env.PORT || 3003); // First parameter = heroku decides the port
+
+
+
+
